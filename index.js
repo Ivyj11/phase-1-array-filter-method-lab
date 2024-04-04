3fd19e81c608532a4c7fb3f43cd5e226eded4013
Ivy <ivyjkemboi@gmail.com>
2024-04-04 23:15:45 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+
+function findMatching(drivers, nameLooked) {
+    return drivers.filter(function (driverName) {
+        return driverName.toLowerCase() === nameLooked.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, letterLooked) {
+    return drivers.filter(function (driverName) {
+        return driverName.toLowerCase().startsWith(letterLooked.toLowerCase());
+    });
+}
+
+function matchName(drivers, nameLooked) {
+        return drivers.filter(function (driver) {
+            return driver.name === nameLooked;
+        });
+    }
